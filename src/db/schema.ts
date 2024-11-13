7341d3d5ce8bbe31ca3f494ebc45e3a51638b5f7
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-11-13 14:13:34 -0300

    add tables
    adicionei algumas tabelas no schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,29 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, foreignKey, decimal } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  user: varchar({ length: 255 }).notNull(),
+  username: varchar({ length: 255 }).notNull(),
   password: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
+
+
+export const accountsTable = pgTable("accounts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer().notNull().references(() => users.id),
+  accountNumber: varchar({ length: 20 }).notNull().unique(),
+  balance: decimal({ precision: 10, scale: 2 }).default('0').notNull(),
+  accountType: varchar({ length: 50 }).notNull(),
+});
+
+export const transactionsTable = pgTable("transactions", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  fromAccountId: integer().notNull().references(() => accountsTable.id),
+  toAccountId: integer().notNull().references(() => accountsTable.id),
+  amount: decimal({ precision: 10, scale: 2 }).notNull(),
+  transactionDate: varchar({ length: 50 }).notNull(), // This could be timestamp
+  description: varchar({ length: 255 }),
+}, (table) => ({
+  fkFromAccount: foreignKey({ columns: [table.fromAccountId], foreignColumns: [accountsTable.id] }),
+  fkToAccount: foreignKey({ columns: [table.toAccountId], foreignColumns: [accountsTable.id] }),
+}));
