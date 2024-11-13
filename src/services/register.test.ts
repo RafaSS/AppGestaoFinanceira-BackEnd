dae4b7b8f196ac391cf3c3768b6d07391e987502
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-11-13 16:38:06 -0300

    add hash test

--- src/services/register.test.ts
+++ src/services/register.test.ts
@@ -16,6 +16,36 @@ describe('RegisterUseCase', () => {
 
         expect(response.user.username).toBe(username)
         expect(response.user.email).toBe(email)
-    }
-    )
+    })
+    
+    test('should hash the password', async () => {
+        const usersRepository = new InmemoryUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+
+        const username = 'user'
+        const email = 'pedrim@gmail'
+        const password = '123'
+
+        const response = await registerUseCase.execute({ username, email, password })
+
+        expect(response.user.password_hash).not.toBe(password)
+    })
+
+    // test('should not register a user with the same email', async () => {
+    //     const usersRepository = new InmemoryUsersRepository()
+    //     const registerUseCase = new RegisterUseCase(usersRepository)
+
+    //     const username = 'user'
+    //     const email = 'pedrim@gmail'
+    //     const password = '123'
+
+    //     await registerUseCase.execute({ username, email, password })
+
+    //     try {
+    //         await registerUseCase.execute({ username, email, password })
+    //     } catch (error) {
+    //         expect(error.message).toBe('User already exists')
+    //     }
+    // })
 })
+
