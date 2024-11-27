12e334ad598dde790dd2bdff0a8e0fbd3497c28c
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-11-27 09:58:43 -0300

    update transaction routes to use FindAllController instead of GetAllController

--- src/http/controllers/transaction/routes.ts
+++ src/http/controllers/transaction/routes.ts
@@ -1,13 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { CreateTransactionController } from './create.controller'
 import { FindOneController } from './findOne.controller';
-// import { GetAllController } from './getAll.controller';
+import { FindAllController } from './getAll.controller';
 
 
 export async function userRoutes(app: FastifyInstance) {
 
     app.post('/users', CreateTransactionController)
-    // app.get('/users', GetAllController)
+    app.get('/users', FindAllController)
     app.get('/users/:id', FindOneController)
 
 }
